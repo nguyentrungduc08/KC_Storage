0db398b4ed32c1e062187ce165ef701d51910398
nguyentrungduc <nguyentrungduc08@gmail.com>
2018-07-02 09:09:20 +0700

    add thrift sources

--- kc_storage.thrift
+++ kc_storage.thrift
@@ -1,6 +1,14 @@
 namespace cpp KC_Storage
 
-service KC_Storage {
-	
+enum putOption {
+	add,
+	update,
+	remove
+}
 
+
+service KC_Storage {
+	string get(1: string key),
+	bool put(1: string key, 2: string value, 3: putOption opt),
+	bool remove (1: string key)
 }
